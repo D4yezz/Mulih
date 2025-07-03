56b897fd3b27e62bcaa18ec302e7ab715f3ae322
Dayezzz <adiasmuhsin1206@gmail.com>
2025-07-03 20:47:02 +0700

    fix directory

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
   resolve: {
     alias: {
       "@": path.resolve( "./src"),
