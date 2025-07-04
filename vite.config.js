f99d86fbaf633240dc397284e98cc4127a9bf426
Dayezzz <adiasmuhsin1206@gmail.com>
2025-07-04 14:11:57 +0700

    add json

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import path from "path"
+import path, { dirname } from "path"
 // import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve( "./src"),
+      "@": path.resolve( __dirname, "./src"),
     },
   },
 })
